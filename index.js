1370512ef2361b1962c95b0ed07d352378919ee9
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-05 09:03:52 +0600

    added product api

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");  // Importing the express module
 const cors = require("cors");
 require('./db/config'); // connection with database
 const User = require('./db/User') // User model
+const Product = require('./db/Product')
 const app = express();  // Initializing the express
 app.use(express.json()); // for converting data(from frontend/postman) to json
 app.use(cors());
+
+
 
 app.post("/register", async (req, resp) => {
     let user = new User(req.body);
@@ -15,7 +18,6 @@ app.post("/register", async (req, resp) => {
 })
 
 app.post("/login", async (req, resp) => {
-
     if (req.body.password && req.body.email) {
         let user = await User.findOne(req.body).select("-password");
 
@@ -25,14 +27,19 @@ app.post("/login", async (req, resp) => {
         else {
             resp.send({ result: 'No User Found' })
         }
-
     }
-
     else {
         resp.send({ result: 'No User Found' })
     }
-
 })
+
+app.post("/add-product", async (req, resp) => {
+    const product = new Product(req.body);
+    let result = await product.save();
+    resp.send(result);
+    // resp.send("product api")
+})
+
 
 
 
